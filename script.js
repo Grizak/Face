d034cc8a98159b799751631bfcfedfc19e6abf74
Isak Grönlund <135023164+Grizak@users.noreply.github.com>
2024-06-04 06:16:57 +0000

    dsoa

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function lock () {
-	document.querySelector('#mouth').classList.add('locked');
+	document.querySelector('#mouth').classList.add('open');
 }
 
 function unlock () {
-	document.querySelector('#mouth').classList.remove('locked')
+	document.querySelector('#mouth').classList.remove('open')
+}
+
+function closeeye () {
+	document.querySelector('#eye').classList.add('closed')
+}
+
+function openeye () {
+	document.querySelector('#eye').classList.remove('closed')
 }
